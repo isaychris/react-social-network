57944a4c3931939dfa0b55470b80500d06544543
isaychris <adaptxsurvive@gmail.com>
2018-10-28 03:50:51 -0700

    fix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/react/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 // catch 404 and forward to error handler
